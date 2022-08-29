89308401a3b4d4b450d9248f6ffdb010ce5b1930
vasanthlmsace <vasanthr.lmsace@gmail.com>
2022-08-29 14:13:24 +0530

    Fixed the js.

--- amd/src/authmagic.js
+++ amd/src/authmagic.js
@@ -41,7 +41,6 @@
         if (params.hascourseregister) {
             self.courseQuickRegistration(params);
         }
-        return true;
     };
 
     AuthMagic.prototype.courseQuickRegistration = function(params) {
